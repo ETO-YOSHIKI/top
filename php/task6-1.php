580bde4d329d00f613dc26ce5414f125fb1205c9
ETO-YOSHIKI <sjetsp02111138@gmail.com>
2024-08-09 17:20:00 +0900

    6-1完成

---
+++ php/task6-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP練習</title>
+</head>
+<body>
+  <?php
+
+
+function showString($num, $str) {
+    for ($i = 0; $i < $num; $i++) {
+        echo $str . "\n<br>";
+    }
+}
+
+showString(3, "気合いだ!");
+
+?>
+
+</body>
+
+</html>
